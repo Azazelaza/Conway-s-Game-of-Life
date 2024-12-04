d18de85423d5b66c1e47171b3523438d7306cf29
Azazel Araiza <azafer150@gmail.com>
2024-12-03 22:47:43 -0600

    Create Component Buttons

--- src/components/layout/ButtonsGame.tsx
+++ src/components/layout/ButtonsGame.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
+
 
 export default function ButtonsGame({
     setIsRunning,
     isRunning,
     resetGame
 }: any) {
-    return (
-        <>
-            <h1>Conway's Game of Life</h1>
-            <button
-                className='bg-gray-200 text-black p-2'
-                onClick={() => setIsRunning(!isRunning)}>
-                {isRunning ? 'Pausar' : 'Iniciar'}
-            </button>
-            <button onClick={resetGame}>Reiniciar</button>
+    const [value, setValue] = useState<number>(10);
+    const Running = ["Iniciar", "Pausar"];
 
-        </>
+    return (
+        <div className='absolute bg-gray-900 right-0 p-6 w-1/3'>
+            <h1 className='text-xl mb-4'>Conway's Game of Life</h1>
+
+            <div className='border rounded p-4 mb-4'>
+                <p className='text-xl'>Tiempo</p>
+                1 Movimiento cada {value} ms
+                <input id="default-range" type="range" min={10} max={5000} onChange={e => setValue(Number(e.target.value))} value={value} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+            </div>
+            <button
+                className='bg-gray-200 w-full text-black p-2 rounded-lg mb-4'
+                onClick={() => setIsRunning(!isRunning)}>
+                {Running[Number(isRunning)]}
+            </button>
+            <button className='bg-gray-200 w-full text-black p-2 rounded-lg' onClick={resetGame}>
+                Reiniciar
+            </button>
+        </div>
     )
 }
